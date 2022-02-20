1b5972d8cf051ff158fcc2fc73c75116c9da2a72
Guiwoo <park.guiwoo@hotmail.com>
2022-02-21 01:03:01 +0900

    blockcahin initalize

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/guiwoo/gocoin/blockchain"
+)
+
+func main() {
+	chain := blockchain.GetBlockChain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("\nData: %s\n", block.Data)
+		fmt.Printf("Data: %s\n", block.Hash)
+		fmt.Printf("Data: %s\n", block.PrevHash)
+	}
 }
