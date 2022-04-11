8269846bc3848fc7dba4ce0185e637fa22b0157f
Guiwoo <park.guiwoo@hotmail.com>
2022-04-11 23:07:12 +0900

    peers peer refetoring before putting blocks

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"github.com/guiwoo/gocoin/cli"
-	"github.com/guiwoo/gocoin/db"
+	"fmt"
 )
 
 func main() {
-	defer db.Close()
-	cli.Start()
+	// defer db.Close()
+	// cli.Start()
+	a := 0
+	b := 0
+	result := 0
+	for i := 1; i < 54; i++ {
+		a = (8900 + (9 * i))
+		b = (1250 + (3 * i * i))
+		result = a - b
+		if result < 0 {
+			fmt.Println("ho", i)
+		}
+	}
 }
